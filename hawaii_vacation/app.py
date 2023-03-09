c6beee05be8f0105fb48d2676d21449f017bbd2b
ysgorin <ysgorin@gmail.com>
2023-03-08 23:30:43 -0500

    added precipiation, stations, and tobs routes

--- hawaii_vacation/app.py
+++ hawaii_vacation/app.py
@@ -4,6 +4,8 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+import datetime as dt
+import numpy as np
 
 # Database Setup
 
@@ -25,6 +27,7 @@ station = Base.classes.station
 app = Flask(__name__)
 
 # Flask Routes
+# Homepage
 @app.route("/")
 def welcome():
     """List all available api routes."""
@@ -33,9 +36,68 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end><br/>"
+        f"/api/v1.0/start_date<br/>"
+        f"/api/v1.0/start_date/end_date<br/>"
     )
+# Precipitation
+@app.route("/api/v1.0/precipitation")
+def prcp():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Last 12 Months of Precipitation Data"""
+    # Query last 12 months of precipitation data
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(precipitation.date, precipitation.prcp).\
+    filter(precipitation.date >= query_date)
+
+    session.close()
+
+    # Convert query results into a dictionary
+    prcp_list = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict['date'] = date
+        prcp_dict['precipitation'] = prcp
+        prcp_list.append(prcp_dict)
+
+    return jsonify(prcp_list)
+
+# Stations
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """List of Stations"""
+    # Query list of stations
+    results = session.query(station.station).\
+    group_by(station.station).all()
+
+    session.close()
+    # Convert query into list
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
+
+# Temperature Observations
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Last 12 Months of Temperature Data for Station 'USC00519281'"""
+    # Query last 12 months of temperature data for most active station 'USC00519281'
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(precipitation.tobs).\
+    filter(precipitation.station == 'USC00519281', precipitation.date >= query_date)
+
+    session.close()
+    # Convert query into list
+    tobs_list = list(np.ravel(results))
+
+    return jsonify(tobs_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
